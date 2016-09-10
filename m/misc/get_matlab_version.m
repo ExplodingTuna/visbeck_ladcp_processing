5258ebe2ce42155371bfa45eb74ce47aabda82fa
Pedro Pena <pedro.pena@noaa.gov>
2016-09-10 07:11:19 -0400

    added is_octave.m to test for octave

--- m/misc/get_matlab_version.m
+++ m/misc/get_matlab_version.m
@@ -1,9 +1,14 @@
-function val = get_matlab_version
+function retval = get_matlab_version
 % GET_MATLAB_VERSION approximates and returns the version of matlab
 % being used.
-  vv = version;
-  ind = findstr(vv,'.');
-  v1 = str2num(vv(1:ind(1)-1));
-  v2 = str2num(vv(ind(1)+1:ind(2)-1));
-  val= str2num([num2str(v1),'.',num2str(v2)]);
+vv = version;
+ind = findstr(vv,'.');
+v1 = str2num(vv(1:ind(1)-1));
+v2 = str2num(vv(ind(1)+1:ind(2)-1));
+val= str2num([num2str(v1),'.',num2str(v2)]);
+if is_octave < 1
+    retval=val;
+else
+    retval=7.2;
+end
 end
