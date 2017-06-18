db9d9e84c48b5a7cd319c7f5007a8ef23a491a41
pedrolpena <pedro.pena@noaa.gov>
2017-06-18 23:34:34 +0000

    added comments

--- m/misc/rmc2nav.m
+++ m/misc/rmc2nav.m
@@ -1,7 +1,13 @@
 function rmc2nav(inFile,outFile)
+%    rmc2nav converts a text file of properly formtted RMC Strings into nav data
+%    that the Visbeck LADCP processing program can use.
+%    this script was written in vectorized form to decrease processing time.
+%
+%    rmc2nav(inFile,outFile)
+%    
 
-%format long g;
 fidout = fopen(outFile,'w');
+% parse file 
 [a,gCtime,c,Clat,ClatDir,Clon,ClonDir,h,i,gCdate,k,l]=textread(inFile,'%s %s %s %s %s %s %s %s %s %s %s %s','delimiter',',');
 
 gTime=cell2mat(gCtime);
@@ -16,7 +22,7 @@ gMonth=str2num(gDate(:,3:4));
 gYear=str2num(gDate(:,5:6));
 gYear=gYear + 2000;
 
-
+% Convert lattitude to decimal lattitude
 lat=cell2mat(Clat);
 latI=str2num(lat(:,1:2));
 latD=str2num(lat(:,3:9));
@@ -25,6 +31,7 @@ latDir=cell2mat(ClatDir);
 latNegVal=find(latDir == 'S');
 decLat(latNegVal)=decLat(latNegVal).* -1;
 
+% Convert longitude to decimal longitude 
 lon=cell2mat(Clon);
 lonI=str2num(lon(:,1:3));
 lonD=str2num(lon(:,4:10));
@@ -32,6 +39,8 @@ decLon=lonI+lonD/60;
 lonDir=cell2mat(ClonDir);
 lonNegVal=find(lonDir == 'W');
 decLon(lonNegVal)=decLon(lonNegVal).* -1;
+
+% Convert time to Julian time 
 tempdate = datenum(gYear,gMonth,gDay,gHour,gMinutes,gSeconds);
 refdate  = datenum(gYear-1,12,31,0,0,0 );
 gooddate = (tempdate-refdate-1);
